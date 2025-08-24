918a36ddd391a806be4f80c4f9f3b823bf93d02b
Rehan sharif nurle <163973645+Rehannurle@users.noreply.github.com>
2025-08-24 14:07:51 +0530

    Update fomo.py

--- fomo.py
+++ fomo.py
@@ -111,7 +111,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(512), nullable=False)
     age = db.Column(db.Integer, nullable=True)
     gender = db.Column(db.String(20), nullable=True)
     is_admin = db.Column(db.Boolean, default=False)  # New admin flag
